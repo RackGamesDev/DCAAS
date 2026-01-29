53a68795272a27452a6c8db7a63853f9ba28cbf7
ystrtnkl <freewillfooljoy@gmail.com>
2026-01-29 19:14:09 +0100

    Avances iniciales

--- comandos.sh
+++ comandos.sh
@@ -13,6 +13,7 @@ cp .env.example .env
 php artisan key:generate
 php artisan migrate
 composer dump-autoload
+#npm install
 
 #iniciar laravel
 #con apache se sirve la carpeta public
@@ -20,6 +21,9 @@ composer dump-autoload
 #inicializar rutas /api
 php artisan install:api
 php artisan route:clear
+
+#inicializar sistema de archivos
+php artisan storage:link
 
 #reestablecer (comandos tipicos para cache y arreglos)
 php artisan migrate
@@ -29,19 +33,38 @@ php artisan route:cache
 php artisan optimize
 php artisan optimize:clear
 
+#conectar a bases de datos sql o redis
+#mirar .env.example
+
+#instalar telescope para debugging
+composer require laravel/telescope --dev
+php artisan telescope:install
+php artisan migrate
+
 #migrar
 php artisan make:migration create_encuestas_table #crea el archivo de migracion para dicha tarea
 php artisan migrate #aplica las migraciones a la base de datos
 #php artisan migrate:rollback #devuelve la base de datos al estado anterior
 php artisan migrate:status
 #php artisan migrate:fresh #borrar todo
-
-#instalacion base
-
+#php artisan migrate:reset down() a todas las migraciones
+php artisan schema:dump #fusiona los archivos de migracion existentes
 
 #instalar paquete
+composer require spatie/laravel-permission
 
+#debug
+composer require spatie/laravel-tail --dev
+tail -f storage/logs/laravel.log #abre una consola interactiva para debugging
+#lanzar mensaje
+#use Illuminate\Support\Facades\Log;
+#Log::info('Checking the user data', ['user' => $user]);
 
-#conectar con la db
-
+#mandar a produccion (obviando comandos anteriores)
+composer install --optimize-autoloader --no-dev
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+#APP_DEBUG=false APP_ENV=production en .env
+#php artisan serve
 
--- varwwwhtml/dcaas-app/routes/api.php
+++ varwwwhtml/dcaas-app/routes/api.php
@@ -2,7 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
+
+//Este archivo es api.php, todas las rutas empezarán por /api
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//RUTAS PÚBLICAS
+
+//Ruta de ejemplo, ante GET /api/test manda un json con esos datos y código 200
+Route::get('/test', function (Request $request) {
+    Log::info('Test', ['user' => "???"]); //Lanzar mensaje a la consola "tail -f storage/logs/laravel.log"
+    return response()->json([
+        'status' => 'success',
+        'message' => 'Funciona correctamente',
+        'timestamp' => now()
+    ], 200);
+});
+
+//RUTAS PRIVADAS
+//hacer antes los middlewares
