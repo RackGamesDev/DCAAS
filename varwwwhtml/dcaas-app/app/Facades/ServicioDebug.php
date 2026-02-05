6675660ffff1e6df51377dcacd7f3102499e408a
ystrtnkl <freewillfooljoy@gmail.com>
2026-02-05 19:50:46 +0100

    Arreglar estructura del proyecto para empezar

---
+++ varwwwhtml/dcaas-app/app/Facades/ServicioDebug.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class ServicioDebug extends Facade
+{
+    protected static function get()
+    {
+        return 'prueba';
+    }
+}
