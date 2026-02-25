ae227006ce9fbc22e9e957c5ce277efbaa5a375e
RackGamesDev <yesterdiscord7@gmail.com>
2026-02-25 01:03:02 +0100

    Documentado

--- docs/post/after.js
+++ docs/post/after.js
@@ -1,3 +1,20 @@
 try{const responseBody = insomnia.response.json();
 const token = responseBody?.data?.access_token ?? false;
 if (token) insomnia.environment.set("AuthorizationToken", token);}catch(e){}
+/*
+
+insomnia.test("Correcto", () => {
+	insomnia.expect(insomnia.response.code).to.equal(200);
+});
+
+*/
+
+/*
+const idEncuesta = responseBody?.data?.encuesta?.id ?? false;
+if (idEncuesta) insomnia.environment.set("encuesta-presentacion", idEncuesta);
+*/
+
+/*
+const idInforme = responseBody?.data?.informe_id ?? false;
+if (idInforme) insomnia.environment.set("informe-presentacion", idInforme);
+*/
